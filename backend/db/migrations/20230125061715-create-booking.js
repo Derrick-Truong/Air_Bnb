412751ee3eb9511e0f35539c8064f03ac76a6364
Derrick-Truong <dtruong169@gmail.com>
2023-01-27 22:36:52 -0800

    pls

--- backend/db/migrations/20230125061715-create-booking.js
+++ backend/db/migrations/20230125061715-create-booking.js
@@ -18,7 +18,7 @@ module.exports = {
         references: {
            model: 'Spots',
            key: 'id' },
-           onDelete: 'CASCADE'
+         onDelete: 'CASCADE'
       },
       userId: {
         type: Sequelize.INTEGER,
