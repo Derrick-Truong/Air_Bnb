2639b01b0eafdc1eef18f5b6f923b905de01d306
Derrick-Truong <dtruong169@gmail.com>
2023-02-10 00:35:23 -0800

    nice

---
+++ frontend/src/context/Modal.js
@@ -0,0 +1,21 @@
+// frontend/src/context/Modal.js
+import React, { useRef } from 'react';
+
+const ModalContext = React.createContext();
+
+export function ModalProvider({ children }) {
+    const modalRef = useRef();
+
+    const contextValue = {
+        modalRef, // reference to modal div
+    };
+
+    return (
+        <>
+            <ModalContext.Provider value={contextValue}>
+                {children}
+            </ModalContext.Provider>
+            <div ref={modalRef} />
+        </>
+    );
+}
