bf8235d2f36eb63af5a073372995edd27d159246
Derrick-Truong <dtruong169@gmail.com>
2023-02-24 22:54:39 -0800

    please

--- backend/db/seeders/20230125062322-demo-spotImages.js
+++ backend/db/seeders/20230125062322-demo-spotImages.js
@@ -13,27 +13,27 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         spotId: 1,
-        url: "https://a0.muscache.com/im/pictures/8fa2e8aa-fecd-440b-91fc-ad97960b92d9.jpg?im_w=960",
+        url: "https://i.imgur.com/emVc2cT.png",
         preview: true
       },
       {
         spotId: 2,
-        url: "https://a0.muscache.com/im/pictures/ecd58908-fbe3-44bd-9a1d-3c95b6742b0c.jpg?im_w=1200",
+        url: "https://i.imgur.com/emVc2cT.png",
         preview: true
       },
       {
         spotId: 3,
-        url: "https://a0.muscache.com/im/pictures/d3532f3b-5dbe-45b3-b63f-ef4a08355408.jpg?im_w=1200",
+        url: "https://i.imgur.com/emVc2cT.png",
         preview: true
       },
       {
         spotId: 4,
-        url: "https://a0.muscache.com/im/pictures/miso/Hosting-48381226/original/88d44ac7-cfa2-432f-8e7e-37fdef42a2e7.jpeg?im_w=1200",
+        url: "https://i.imgur.com/emVc2cT.png",
         preview: true
       },
       {
         spotId: 5,
-        url: "https://a0.muscache.com/im/pictures/9b03dbf5-2800-493d-8451-40113cf1e65b.jpg?im_w=1200",
+        url: "https://i.imgur.com/emVc2cT.png",
         preview: true
       }
     ], {});
@@ -43,7 +43,7 @@ module.exports = {
     options.tableName = "SpotImages";
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      url: { [Op.in]: ["url-path1", "url-path2", "url-path3"] }
+      url: { [Op.in]: ["https://i.imgur.com/emVc2cT.png", "https://i.imgur.com/emVc2cT.png", "https://i.imgur.com/emVc2cT.png"] }
     }, {});
   }
 };
